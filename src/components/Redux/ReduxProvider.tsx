a496f29d4fa41429851ac3744ee4ca077f1158f8
dev-ninja-07 <152303121+dev-ninja-07@users.noreply.github.com>
2025-04-25 15:45:54 +0900

    Update ReduxProvider.tsx

--- src/components/Redux/ReduxProvider.tsx
+++ src/components/Redux/ReduxProvider.tsx
@@ -5,6 +5,6 @@ import store from "@/store";
 
 const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
   return <Provider store={store}>{children}</Provider>;
-};
+}; 
 
 export default ReduxProvider;
